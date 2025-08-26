6f14c6909380cc14ae47b862d7a7e6f8bd840bc7
unknown <180252894+piaohudev@users.noreply.github.com>
2025-08-26 13:26:48 +0200

    chains and langchain expression language - LCEL

--- langchain_intro/chatbot.py
+++ langchain_intro/chatbot.py
@@ -1,10 +1,60 @@
 import dotenv
 from langchain_openai import ChatOpenAI
+from langchain.prompts import (
+    PromptTemplate,
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+)
+from langchain_core.output_parsers import StrOutputParser
 
 dotenv.load_dotenv()
 
-chat_model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+review_template_str = """
+    Your job is to use patient reviews to answer questions about their experience at a hospital.
+    Use the following context to answer questions.
+    Be as detailed as possible, but don't make up any information that's not from the context.
+    If you don't know an answer, say you don't know.
+
+    {context}
+"""
+
+review_system_prompt = SystemMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["context"],
+        template=review_template_str,
+    )
+)
+review_human_prompt = HumanMessagePromptTemplate(
+    prompt=PromptTemplate(
+        input_variables=["question"],
+        template="{question}",
+    )
+)
+messages = [review_system_prompt, review_human_prompt]
+
+review_prompt_template = ChatPromptTemplate(
+    input_variables=["context", "question"],
+    messages=messages,
+)
+
+chat_model = ChatOpenAI(
+    model="gpt-3.5-turbo-0125",
+    temperature=0,
+)
+
+output_parser = StrOutputParser()
+
+review_chain = review_prompt_template | chat_model | output_parser
+
 
 if __name__ == '__main__':
-    response = chat_model.invoke("What is blood pressure?")
-    print(response.content)
+    context = "I had a great stay!"
+    question = "Did anyone have a positive experience?"
+
+    response = review_chain.invoke({
+        "context": context,
+        "question": question,
+    })
+
+    print(response)
