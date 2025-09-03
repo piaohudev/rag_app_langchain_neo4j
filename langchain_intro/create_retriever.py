a4f2c82c63273fd61117fea38b1023bb19a28394
unknown <180252894+piaohudev@users.noreply.github.com>
2025-09-03 18:38:27 +0200

    migrate old source files

--- langchain_intro/create_retriever.py
+++ langchain_intro/create_retriever.py
@@ -8,30 +8,9 @@ REVIEWS_CHROMA_PATH = "chroma_data"
 
 dotenv.load_dotenv()
 
-loader = CSVLoader(
-    file_path=REVIEWS_CSV_PATH,
-    source_column="review",
-)
+loader = CSVLoader(file_path=REVIEWS_CSV_PATH, source_column="review")
 reviews = loader.load()
 
 reviews_vector_db = Chroma.from_documents(
     reviews, OpenAIEmbeddings(), persist_directory=REVIEWS_CHROMA_PATH
 )
-
-test_reviews_vector_db = Chroma(
-    persist_directory="chroma_data/",
-    embedding_function=OpenAIEmbeddings(),
-)
-
-if __name__ == '__main__':
-    question = """
-        Has anyone complained about communication with the hospital staff?
-    """
-
-    relevant_docs = test_reviews_vector_db.similarity_search(question, k=3)
-
-    response = ""
-    for doc in relevant_docs:
-        response += f"{doc.page_content}\n================================\n"
-
-    print(response)
